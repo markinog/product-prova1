f041c557d3908f9b1dea24660d20870350299f42
markinog <marcussla9@gmail.com>
2026-03-30 20:07:01 -0300

    ajuste

--- src/main/java/com/ecommerce/product/entity/Product.java
+++ src/main/java/com/ecommerce/product/entity/Product.java
@@ -1,12 +1,10 @@
 package com.ecommerce.product.entity;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import tools.jackson.databind.deser.jdk.StringArrayDeserializer;
 
 import java.math.BigDecimal;
 
